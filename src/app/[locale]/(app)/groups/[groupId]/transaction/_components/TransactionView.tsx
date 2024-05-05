5da22b62967780ded31a238b85d0aa9f665621f1
Talha Bayansar <talha.bayansar@hotmail.com>
2024-05-05 20:52:58 +0200

    adding success and error states to transaction

--- src/app/[locale]/(app)/groups/[groupId]/transaction/_components/TransactionView.tsx
+++ src/app/[locale]/(app)/groups/[groupId]/transaction/_components/TransactionView.tsx
@@ -5,17 +5,36 @@ import { View } from "@/components/layout/View";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useGroupById } from "@/groups/hooks/useGroupById";
-import { ArrowRightLeft } from "lucide-react";
+import { useGroupPointsByGroupId } from "@/groups/hooks/useGroupPointsByGroupId";
+import { createTransaction } from "@/groups/service";
+import { useMutation } from "@tanstack/react-query";
+import { ArrowRightLeft, Check, X } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { useParams, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export const TransactionView = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
   const t = useTranslations("global");
   const tTransactionPage = useTranslations("TransactionPage");
   const { groupId } = useParams<{ groupId: string }>();
   const searchParams = useSearchParams();
   const amount = searchParams.get("amount") as string;
   const { data, isLoading } = useGroupById(groupId);
+  const { refetch } = useGroupPointsByGroupId(Number(groupId));
+  const mutation = useMutation({
+    mutationFn: async () => {
+      await createTransaction(Number(groupId), Number(amount));
+    },
+    onError: () => {
+      setError(true);
+    },
+    onSuccess: async () => {
+      setSuccess(true);
+      await refetch();
+    },
+  });
 
   if (isLoading) return <TransactionViewSkeleton />;
 
@@ -23,6 +42,9 @@ export const TransactionView = () => {
     return (
       <EmptyView Icon={ArrowRightLeft} message={tTransactionPage("no_group")} />
     );
+
+  if (error) return <ErrorView />;
+  if (success) return <SuccessView />;
 
   return (
     <View className="items-center justify-between flex-grow">
@@ -35,7 +57,31 @@ export const TransactionView = () => {
         </View>
       </View>
 
-      <Button className="w-full">{t("submit")}</Button>
+      <Button className="w-full" onClick={() => mutation.mutate()}>
+        {t("submit")}
+      </Button>
+    </View>
+  );
+};
+
+const SuccessView = () => {
+  const t = useTranslations("TransactionPage");
+
+  return (
+    <View className="items-center justify-center flex-grow">
+      <Check size={60} className="text-green-600" />
+      <h2 className="text-xl font-medium text-center">{t("success")}</h2>
+    </View>
+  );
+};
+
+const ErrorView = () => {
+  const t = useTranslations("TransactionPage");
+
+  return (
+    <View className="items-center justify-center flex-grow">
+      <X size={60} className="text-destructive" />
+      <h2 className="text-xl font-medium text-center">{t("error")}</h2>
     </View>
   );
 };
