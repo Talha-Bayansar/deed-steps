8ad11a410904934a7c07e22b56f269b7917a9044
Talha Bayansar <talha.bayansar@hotmail.com>
2024-12-09 17:18:12 +0100

    adding scanner modal

--- src/app/[locale]/(app)/groups/[groupId]/_components/GroupDetailsView.tsx
+++ src/app/[locale]/(app)/groups/[groupId]/_components/GroupDetailsView.tsx
@@ -10,18 +10,26 @@ import { GroupMembersView } from "@/features/groups/components/GroupMembersView"
 import { useGroupById } from "@/features/groups/hooks/useGroupById";
 import { useGroupPointsByGroupId } from "@/features/groups/hooks/useGroupPointsByGroupId";
 import { routes } from "@/lib/routes";
-import { Users, Settings, Coins } from "lucide-react";
-import { Link } from "@/navigation";
+import { Users, Settings, Coins, Camera } from "lucide-react";
+import { Link, useRouter } from "@/navigation";
 import { useParams } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { BackButton } from "@/components/BackButton";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
 
 export const GroupDetailsView = () => {
   const t = useTranslations("global");
+  const router = useRouter();
   const tGroupDetailspage = useTranslations("GroupDetailsPage");
   const { groupId } = useParams<{ groupId: string }>();
   const { data, isLoading } = useGroupById(groupId);
   const { data: points } = useGroupPointsByGroupId(Number(groupId));
+
+  const handleScan = (values: IDetectedBarcode[]) => {
+    const value = values[0];
+    router.push(value.rawValue);
+  };
 
   if (isLoading) return <GroupDetailsViewSkeleton />;
 
@@ -39,6 +47,16 @@ export const GroupDetailsView = () => {
           <div className="flex items-center gap-2 bg-gray-50 rounded p-2">
             <Coins className="text-yellow-400" /> {points?.points ?? "?"}
           </div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <IconButton>
+                <Camera className="text-primary" />
+              </IconButton>
+            </DialogTrigger>
+            <DialogContent>
+              <Scanner onScan={handleScan} allowMultiple={false} />
+            </DialogContent>
+          </Dialog>
           {data.isOwner && (
             <Link href={routes.groups.id(groupId).settings.root}>
               <IconButton>
