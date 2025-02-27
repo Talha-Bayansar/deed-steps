a84423c464e280d1caff0c0626d23f109a26e301
Talha Bayansar <talha.bayansar@hotmail.com>
2025-02-27 21:30:29 +0100

    adding smooth scroll to landing page only

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -9,7 +9,7 @@ import CTA from "./_components/cta";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="block scroll-smooth overflow-y-scroll h-screen">
       <Header />
       <main className="flex-1">
         <Hero />
